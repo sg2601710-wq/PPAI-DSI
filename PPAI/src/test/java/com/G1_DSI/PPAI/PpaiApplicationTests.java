68a78fe2d838250e4fc06dd18a2d277e46a2ab89
Freites Joaquin Valentino - 408476 <408476@sistemas.frc.utn.edu.ar>
2026-06-30 17:02:41 -0300

    Se configuró y reorganizó el proyecto, realizandolo correctamente con SpringBoot.

---
+++ PPAI/src/test/java/com/G1_DSI/PPAI/PpaiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.G1_DSI.PPAI;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PpaiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
